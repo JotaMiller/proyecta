0cbe1966611770af9a059124b812847445fb741e
Jimmy Miller <jotamiller@JotaMiller.(none)>
2012-11-11 20:17:22 -0300

    Cambios

--- proyeccion/views.py
+++ proyeccion/views.py
@@ -84,13 +84,9 @@ def index(request):
         # Se genera la ruta en la cual se guardara el grafico
         span = settings.PROJECT_PATH + '/media/graficos/' + grafico
     
-        ventas2 = total_ventas(1, fecha_inicio, fecha_termino)
+        detalle_ventas = total_ventas(id_producto, fecha_inicio, fecha_termino)
         
-        ventas = (72,79,77,59,61,80,72,53,60,67,60,63,
-              67,68,67,72,54,74,59,69,80,56,77,78,
-              54,59,51,62,76,60,68,62,67,78,67,58,
-              69,55,73,78,80,73,76,75,63,74,63,65,
-              74,53,73,52,51,67,77,73,63,71,74,64)
+        ventas = detalle_ventas[0]
 
         salida = convert_ts(ventas, 2008, 1,12)
         r('''
@@ -161,7 +157,7 @@ def index(request):
     })
     return render_to_response('proyeccion/index.html',c)
     
-def total_ventas(producto, fecha_inicio, fecha_termino):
+def total_ventas(id_producto, fecha_inicio, fecha_termino):
     """
     Funcion que devuelve las ventas realizadas de un determinado
     producto en un periodo de tiempo, solo devuelve el monto de las ventas
@@ -173,31 +169,47 @@ def total_ventas(producto, fecha_inicio, fecha_termino):
     fecha_inicio = datetime.strptime(fecha_inicio,"%Y-%m-%d") 
     fecha_termino = datetime.strptime(fecha_termino,"%Y-%m-%d") 
     
-    
+    id_producto = int(id_producto)
     tiempo = Tiempo.objects.filter( fecha__range = (fecha_inicio, fecha_termino)).order_by('fecha')
-    ventas = []
+    ventas_ts = []
+    ventas_detalle = []
+    detalle_venta = ""
     
     cont_ano    =   fecha_inicio.year
-
+    cont_mes    =   fecha_inicio.month
+    final_mes   =   12
+    
     while ( cont_ano <= fecha_termino.year ):
-        cont_mes    =   1
-        while ( cont_mes <= 31 ):
+        if cont_ano == fecha_termino.year:
+            final_mes = fecha_termino.month
+        while ( cont_mes <= final_mes ):
         #TODO: falta iteracion para recorrer meses
+            fecha_ventas = Tiempo.objects.filter( fecha__month = cont_mes )
+            
+            venta_total_mes = 0
+            for fecha_venta in fecha_ventas:
+                productos = Producto.objects.filter( venta = fecha_venta.venta )
+                
+                for producto in productos:
+                    if producto.id == id_producto:
+                        #ventas.append([producto.id, id_producto])
+                        venta_total_mes = venta_total_mes + fecha_venta.venta.total_venta
+                
+            ventas_detalle.append({'monto': venta_total_mes, 'mes': cont_mes, 'ano': cont_ano})
+            ventas_ts.append(venta_total_mes)
             cont_mes = cont_mes + 1
+            
         cont_ano = cont_ano + 1
-        
-    for venta in tiempo:
-        ventas.append(venta.fecha)
-        
-    return ventas
+        cont_mes = 1
+    
+    return ventas_ts, ventas_detalle
 
 def convert_ts(time_series, start_year=2000, start_pd=1, freq=12):
      """
      Convierte una serie de datos dentro de un objeto rpy2
      los transforma a un objeto de tipo 'ts'
      """
-     r_ts = robjects.r.ts(robjects.FloatVector(time_series),
-                          start=robjects.r.c(start_year,start_pd), frequency=freq)
+     r_ts = robjects.r.ts(robjects.FloatVector(time_series),start=robjects.r.c(start_year,start_pd), frequency=freq)
      return r_ts
  
 @login_required
